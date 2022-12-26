3f2ada33e0eb878c866804d70329536ca1276580
Yusril Anwar <yusril886@gmail.com>
2022-12-26 16:48:33 +0800

    change code

--- app/src/main/java/com/muasdev/pareparedev/MainActivity.kt
+++ app/src/main/java/com/muasdev/pareparedev/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //yusril
-        println("===Yusril===")
+        Log.d("logTest","===Yusril===")
     }
 }
